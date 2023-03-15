26db879f8219e23ed877307fd6b3107433582ad9
Abraham Huacchillo <huacchillocastillo@gmail.com>
2023-03-15 16:45:01 -0600

    in the search the error

--- server/models/user.go
+++ server/models/user.go
@@ -6,7 +6,7 @@ type User struct {
 	ID        uint   `gorm:"primaryKey" json:"id"`
 	Email     string `gorm:"uniqueIndex" json:"email"`
 	Password  string `json:"-"`
-	FirsName  string `json:"first_name"`
+	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
