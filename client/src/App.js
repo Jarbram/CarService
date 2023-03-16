a6c5fc0ac333e3f9f2ad081a4f07a553e68cf282
Abraham Huacchillo <huacchillocastillo@gmail.com>
2023-03-15 23:45:07 -0600

    add styles for the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
-
-//import Login from './login/Login.jsx';
+import { Switch, Route } from 'react-router-dom';
+import Login from './login/Login.jsx';
 import SignUp from './signup/SignUp.jsx';
+import Home from "./home/Home.jsx";
 
 
 function App() {
   return (
-    <div>
-      <SignUp />
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
   );
 }
 
---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaHamburger } from 'react-icons/fa';
+import './navbar.css';
+
+const Navbar = () => {
+  const [showNav, setShowNav] = React.useState(false);
+
+  const handleMenuClick = () => {
+    setShowNav(!showNav);
+  };
+
+  return (
+    <nav className='container-navbar'>
+      <h1 className='navbar-title'>Jarbram</h1>
+      <div className='btn-loginSignup'>
+        <Link to='/login'>
+          <button className='btn-login'>Login</button>
+        </Link>
+        <Link to='/signup'>
+          <button className='btn-signUp'>Sign Up</button>
+        </Link>
+      </div>
+      <ul className={showNav ? 'navigation open' : 'navigation'}>
+        <li className='navbar-item'>
+          <a href='#services'>Services</a>
+        </li>
+        <li className='navbar-item'>
+          <a href='#projects'>Projects</a>
+        </li>
+        <li className='navbar-item'>
+          <a href='#about'>About</a>
+        </li>
+      </ul>
+      <div className='hamburger' onClick={handleMenuClick}>
+        <FaHamburger />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
