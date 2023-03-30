53933cfd0ac941223dbab442b80db833c13776ab
Abraham Huacchillo <huacchillocastillo@gmail.com>
2023-03-30 14:54:18 -0600

    changes in the user

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,57 +1,57 @@
-
-import './login.css'
-import React, { useState } from "react";
-import axios from "axios";
-import Navbar from '../navbar/Navbar.jsx'
+import React, { useState } from 'react';
+import axios from 'axios';
+import Navbar from '../navbar/Navbar';
+import './login.css';
 
 const Login = ({ setUser }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      const response = await axios.post("http://localhost:3000/login", { email, password });
+      const response = await axios.post('http://localhost:3000/login', { email, password });
       setUser(response.data);
     } catch (error) {
-      setError("Email o contraseña incorrectos");
-      console.error(error);
+      setError('Email o contraseña incorrectos. Por favor, inténtelo de nuevo.');
+      console.error(error.message);
     }
   };
 
   return (
     <>
-    <Navbar />
-    <div className='container'> 
+      <Navbar />
+      <div className='container'>
         <form className='form-login' onSubmit={handleSubmit}>
-          {error && <div className="error">{error}</div>}
+          {error && <div className='error'>{error}</div>}
           <h2 className='title-login'>Iniciar sesión</h2>
           <div className='email-container'>
             <input
-              type="email"
-              id="email"
+              type='email'
+              id='email'
               placeholder='Email'
               value={email}
-              onChange={(event) => setEmail(event.  target.value)}
+              onChange={(event) => setEmail(event.target.value)}
             />
           </div>
           <div className='password-container'>
             <input
-              type="password"
-              id="password"
-              placeholder='Password'
+              type='password'
+              id='password'
+              placeholder='Contraseña'
               value={password}
-              onChange={(event) => setPassword(event.   target.value)}
+              onChange={(event) => setPassword(event.target.value)}
             />
           </div>
-          <button className='btn' type="submit">Iniciar sesión</button>
+          <button className='btn' type='submit'>
+            Iniciar sesión
+          </button>
         </form>
-    </div>
+      </div>
     </>
   );
 };
 
 export default Login;
-
--- server/models/user.go
+++ server/models/user.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	ID        uint   `gorm:"primaryKey" json:"id"`
 	Email     string `gorm:"uniqueIndex" json:"email"`
-	Password  string `json:"-"`
+	Password  string `json:"password"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	CreatedAt time.Time
